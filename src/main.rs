af9cf5d6c0f6209daabcf56fbb0e785a0ed9b4cf
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2020-05-16 15:34:16 +0200

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ fn main() {
     timely::execute_from_args(std::env::args(), move |worker| {
         let mut input = triangles(worker);
 
+        /*
+        3--2----8--9
+        | / \   | /
+        |/   5  |/
+        1--4    7
+        */
         input.advance_to(0);
         let edges = vec![(1, 2), (2, 3), (1, 3), (2, 1), (3, 2), (3, 1), (1, 4),
                          (4, 1), (2, 5), (5, 2), (7, 8), (8, 7), (7, 9), (9, 7),
@@ -18,7 +24,12 @@ fn main() {
         }
 
         input.advance_to(1);
-
+        /*
+        3  2----8--9
+        | /|\   | /
+        |/ | 5  |/
+        1--4    7
+        */
         input.insert((2, 4));
         input.insert((4, 2));
         input.remove((2, 3));
