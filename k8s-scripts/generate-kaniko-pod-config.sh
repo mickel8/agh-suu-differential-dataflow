b89f6a216079c69fae3ed6f8434772736648e5e8
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-05-24 17:57:35 +0200

    Fix in generate-kaniko-pod-config script

--- k8s-scripts/generate-kaniko-pod-config.sh
+++ k8s-scripts/generate-kaniko-pod-config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+FILE=kaniko-pod
+SCHEME=$FILE-scheme
+DIR=k8s-config
+SCHEME_PATH=$DIR/$SCHEME
+FILE_PATH=$DIR/$FILE.yaml
+
 echo -n 'Enter Docker Hub ID: '
 read DOCKERHUB_ID
 echo -n 'Enter Repository name: '
@@ -9,4 +15,4 @@ read DOCKERHUB_REPO_TAG
 sed -e "s/\${DOCKERHUB_ID}/$DOCKERHUB_ID/" \
   -e "s/\${DOCKERHUB_REPO}/$DOCKERHUB_REPO/" \
   -e "s/\${DOCKERHUB_REPO_TAG}/$DOCKERHUB_REPO_TAG/" \
-  k8s-config/kaniko-pod.yaml > k8s-config/kaniko-pod.yaml
+  $SCHEME_PATH > $FILE_PATH
