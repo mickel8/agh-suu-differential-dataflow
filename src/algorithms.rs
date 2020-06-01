f14ae47b04ff1d210c905f11641b3a3f4e9f1344
Michał Śledź <sledzson08@gmail.com>
2020-06-01 13:07:40 +0200

    Add latency benchmarks

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -3,6 +3,9 @@ pub mod graph {
     use differential_dataflow::operators::{Join, Threshold};
     use timely::communication::allocator::generic::Generic;
     use timely::worker::Worker;
+    use timely::dataflow::operators::probe::Handle;
+
+    type IntInputSession = InputSession<i32, (i32, i32), isize>;
 
     fn sort_tuple(tuple: (i32, i32, i32)) -> (i32, i32, i32) {
         let mut as_vec: Vec<i32> = vec!(tuple.0, tuple.1, tuple.2);
@@ -10,10 +13,10 @@ pub mod graph {
         (as_vec[0], as_vec[1], as_vec[2])
     }
 
-    pub fn triangles(worker: &mut Worker<Generic>) -> InputSession<i32, (i32, i32), isize> {
+    pub fn triangles(worker: &mut Worker<Generic>) -> (IntInputSession, Handle<i32>) {
         let mut input: InputSession<i32, (i32, i32), isize> = InputSession::new();
 
-        worker.dataflow(|scope| {
+        let probe = worker.dataflow(|scope| {
             let edges = input.to_collection(scope);
 
             edges
@@ -23,8 +26,9 @@ pub mod graph {
                 .semijoin(&edges)
                 .map(|((x, z), y)| sort_tuple((x, y, z)))
                 .distinct()
-                .inspect(|x| println!("{:?}", x));
+                // .inspect(|x| println!("{:?}", x))
+                .probe()
         });
-        input
+        (input, probe)
     }
 }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,33 +1,42 @@
 use agh_suu_differential_dataflow::algorithms::graph::triangles;
+use rand::prelude::*;
 
 fn main() {
+    let num = 100_000;
+    let edges = rand_edges(num);
     timely::execute_from_args(std::env::args(), move |worker| {
-        let mut input = triangles(worker);
-
-        /*
-        3--2----8--9
-        | / \   | /
-        |/   5  |/
-        1--4    7
-        */
+        let (mut input, probe) = triangles(worker);
         input.advance_to(0);
-        let edges = vec![(1, 2), (2, 3), (1, 3), (2, 1), (3, 2), (3, 1), (1, 4),
-                         (4, 1), (2, 5), (5, 2), (7, 8), (8, 7), (7, 9), (9, 7),
-                         (8, 9), (8, 9), (2, 8), (8, 2)];
-        for (u, v) in edges {
-            input.insert((u, v));
+        let mut i = worker.index() as i32;
+        while i < num / 2 {
+            input.insert(edges[i as usize]);
+            input.advance_to(i);
+            i += worker.peers() as i32;
         }
+        input.advance_to(i);
+        input.flush();
+        while probe.less_than(&input.time()) { worker.step(); }
+        println!("{:?}\t Step 1 (added {} edges)", worker.timer().elapsed(), num / 2 / worker.peers() as i32);
 
-        input.advance_to(1);
-        /*
-        3  2----8--9
-        | /|\   | /
-        |/ | 5  |/
-        1--4    7
-        */
-        input.insert((2, 4));
-        input.insert((4, 2));
-        input.remove((2, 3));
-        input.remove((3, 2));
+        while i < num {
+            input.insert(edges[i as usize]);
+            input.advance_to(i);
+            i += worker.peers() as i32;
+        }
+        input.flush();
+        while probe.less_than(&input.time()) { worker.step(); }
+        println!("{:?}\t Step 2 (added {} edges)", worker.timer().elapsed(), num / 2 / worker.peers() as i32);
     }).expect("Computation terminated abnormally");
+
+    fn rand_edges(num: i32) -> Vec<(i32, i32)> {
+        let mut rng = rand::thread_rng();
+        let mut rand_edges = vec![];
+        let rnd_bound = (num as f32).sqrt().floor() as i32;
+        for _ in 0..num {
+            let v1 = rng.gen_range(0, rnd_bound);
+            let v2 = rng.gen_range(0, rnd_bound);
+            rand_edges.push((v1, v2));
+        }
+        rand_edges
+    }
 }
