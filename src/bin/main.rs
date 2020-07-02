cb41b3244d49772a10133c7756133e90e13a0752
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2020-07-02 15:41:16 +0200

    Intruduce slave/master job model

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,45 +1,66 @@
 use std::env;
 use std::fs;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream, Shutdown};
+use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 
 use differential_dataflow::input::InputSession;
+use spmc;
+use spmc::{Receiver, Sender};
 use timely::communication::allocator::generic::Generic;
+use timely::dataflow::operators::probe::Handle;
 use timely::worker::Worker;
 
 use agh_suu_differential_dataflow::algorithms::graph::triangles;
-use timely::dataflow::operators::probe::Handle;
 use agh_suu_differential_dataflow::Msg;
 
 fn main() {
-    let proc_id = fs::read_to_string("/etc/agh-suu-dd/proc-id.cnf").unwrap().trim_end().to_owned();
-    let processes = env::var("AGH_SUU_DD_CLUSTER_SIZE").unwrap();
-    println!("Process-ID: {}; Cluster size: {}", proc_id, processes);
+    let (tx, rx): (Sender<TcpStream>, Receiver<TcpStream>) = spmc::channel();
 
-    let args = vec!["-w".to_owned(), "1".to_owned(),
-                    "-n".to_owned(), processes.to_owned(),
-                    "-p".to_owned(), proc_id.to_owned(),
-                    "-h".to_owned(), "/etc/agh-suu-dd/hostfile.cnf".to_owned()].into_iter();
-
-    timely::execute_from_args(args, move |worker| {
-        let (mut input, probe) = triangles(worker);
-        let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
-        println!("Server started on worker {}! Waiting on port 7878", worker.index());
-
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            println!("Received a new connection");
-            handle_connection(stream, &mut input, &probe, worker)
-        }
-
-        println!("End computation");
-    }).expect("Computation terminated abnormally");
+    let handle = thread::spawn(move || {
+        run_dd_instance(rx);
+    });
+    run_tcp_server(tx);
+    handle.join().unwrap();
 
     println!("Sleep");
-
     thread::sleep(Duration::from_millis(100000));
+}
+
+fn run_tcp_server(mut sender: Sender<TcpStream>) {
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    println!("Server started! Waiting on port 7878");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Received a new connection");
+        sender.send(stream).unwrap();
+    }
+}
+
+fn run_dd_instance(receiver: Receiver<TcpStream>) {
+    let workers = env::var("AGH_SUU_DD_WORKERS_PER_NODE").unwrap();
+    let processes = env::var("AGH_SUU_DD_CLUSTER_SIZE").unwrap();
+    let proc_id = fs::read_to_string("/etc/agh-suu-dd/proc-id.cnf").unwrap().trim_end().to_owned();
+    let hostfile = "/etc/agh-suu-dd/hostfile.cnf";
+
+    println!("Process-ID: {}; Cluster size: {}", proc_id, processes);
+
+    let args = vec!["-w".to_owned(), workers.to_owned(),
+                    "-n".to_owned(), processes.to_owned(),
+                    "-p".to_owned(), proc_id.to_owned(),
+                    "-h".to_owned(), hostfile.to_owned()].into_iter();
+
+    timely::execute_from_args(args, move |worker| {
+        println!("Worker {} of process {} started", worker.index(), proc_id);
+
+        let (mut input, probe) = triangles(worker);
+        loop {
+            let stream = receiver.recv().unwrap();
+            handle_connection(stream, &mut input, &probe, worker)
+        }
+    }).expect("Computation terminated abnormally");
 }
 
 fn handle_connection(
@@ -48,12 +69,12 @@ fn handle_connection(
     probe: &Handle<i32>,
     worker: &mut Worker<Generic>,
 ) {
-    println!("Waiting for msg");
+    println!("Worker {}: Waiting for msg", worker.index());
     loop {
         let mut buffer = [0; 512];
         let bytes = stream.read(&mut buffer).unwrap();
         if bytes == 0 {
-            println!("Connection closed");
+            println!("Worker {}: Connection closed", worker.index());
             stream.shutdown(Shutdown::Both).unwrap();
             break;
         }
@@ -61,7 +82,7 @@ fn handle_connection(
         while index < bytes {
             let size: u8 = bincode::deserialize(&buffer[index - 1..index]).unwrap();
             let msg: Msg = bincode::deserialize(&buffer[index..(index + size as usize)]).unwrap();
-            println!("Got msg: {}", msg);
+            println!("Worker {}: Got msg: {}", worker.index(), msg);
             compute(msg, input, probe, worker);
             index += size as usize + 1;
         }
